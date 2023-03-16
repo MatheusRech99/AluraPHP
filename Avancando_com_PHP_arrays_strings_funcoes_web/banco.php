9523303f53da0394b9a3b1e73f372cd46e201fbb
MatheusRech99 <matheus.rech99@gmail.com>
2023-03-16 15:13:36 -0300

    Aula 5 - Iniciando em funções

---
+++ Avancando_com_PHP_arrays_strings_funcoes_web/banco.php
@@ -0,0 +1,33 @@
+<?php
+
+function exibeMensagem($mensagem)
+{
+    echo $mensagem . PHP_EOL;
+}
+
+exit();
+$contasCorrentes = [
+    '564.022.852-63' => [
+        'titular' => 'Matheus',
+        'saldo' => 2500
+    ],
+    '654.035.589-53' => [
+        'titular' => 'Magui',
+        'saldo' => 3000
+    ],
+    '460.152.233-68' => [
+        'titular' => 'Gelasio',
+        'saldo' => 250000
+    ]
+];
+
+
+if (500 > $contasCorrentes['654.035.589-53']['saldo']) {
+    exibeMensagem('vai dar nao');
+} else {
+    $contasCorrentes['654.035.589-53']['saldo'] -= 500;
+}
+
+foreach ($contasCorrentes as $cpf => $conta) {
+    exibeMensagem("$cpf {$conta['titular']} R$ {$conta['saldo']} ,00");
+}
